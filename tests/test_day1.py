ae63ea0f7c4ad481a61889f1d77d6b5b57928af8
thirdformant <r01daw13@abdn.ac.uk>
2018-12-05 14:17:13 +0000

    add tests for day 1 part 2

--- tests/test_day1.py
+++ tests/test_day1.py
@@ -13,6 +13,20 @@ class Day1Tests(unittest.TestCase):
         self.assertTrue(day1.freq_addition([-1, -2, -3]) == -6,
                         msg="Addition of frequencies != -6")
 
+    def test_repeat_freq(self):
+        self.assertTrue(day1.repeat_freq([+1, -1]) == 0,
+                        msg="Repeated frequency != 0")
+
+        self.assertTrue(day1.repeat_freq([+3, +3, +4, -2, -4]) == 10,
+                        msg="Repeated frequency != 10")
+
+        self.assertTrue(day1.repeat_freq([-6, +3, +8, +5, -6]) == 5,
+                        msg="Repeated frequency != 5")
+
+        self.assertTrue(day1.repeat_freq([+7, +7, -2, -7, -4]) == 14,
+                        msg="Repeated frequency != 14")
+
+
 def main():
     unittest.main()
 
