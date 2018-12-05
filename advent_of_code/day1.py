42e67b4de28e2573d2fd5b3802e95c1c28124ebc
thirdformant <r01daw13@abdn.ac.uk>
2018-12-05 15:39:22 +0000

    add solution for day 1 part 2

--- advent_of_code/day1.py
+++ advent_of_code/day1.py
@@ -1,28 +1,30 @@
 import os
+from itertools import cycle
+import numpy as np
 
-def file_to_intlist(input_file):
-    """
-    Read input file to list of integers
-    """
-    list = []
-    with open(input_file, "r") as f:
-        for line in f:
-            list.append(int(line.rstrip()))
-    f.close()
-    return list
-
-def freq_addition(list):
+def freq_addition(freq_changes):
     """
     Starting at 0, add value of each item in list and return final result.
     """
     init_freq = 0
-    for freq in list:
+    for freq in freq_changes:
         init_freq += freq
     return init_freq
 
+def repeat_freq(freq_changes):
+    observed_freqs = {0}
+    current_freq = 0
+    for n in cycle(freq_changes):
+        current_freq += n
+        if current_freq in observed_freqs:
+            return current_freq
+        observed_freqs.add(current_freq)
+
 
 if __name__ == "__main__":
-    input_file = os.path.join(os.pardir, "input", "day1", "input.txt")
-    freq_values = file_to_intlist(input_file)
-    freq_addition_result = freq_addition(freq_values)
+    INPUT_PATH = os.path.join(os.pardir, "input", "day1", "input.txt")
+    freq_changes = [int(x) for x in open(INPUT_PATH).readlines()]
+    freq_addition_result = freq_addition(freq_changes)
     print(freq_addition_result) #Print the final result
+    part_2 = repeat_freq(freq_changes)
+    print(part_2)
